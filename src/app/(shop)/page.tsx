74498723a33bc39267a12acd6b739991e1ab1edb
gothmate <gothmate@gmail.com>
2024-08-04 22:35:42 -0300

    feature(Checkout): - criada página de checkout

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -3,15 +3,25 @@ import products from "@/data/constants/products"
 import styles from "../page.module.sass"
 import Page from "../template/Page"
 import ProductCard from "@/components/product/ProductCard"
+import { useContext } from "react"
+import SessionContext from "@/data/context/SessionContext"
 
 export default function Home() {  
+  
+  const session = useContext(SessionContext)
+
   return (
-    <Page className={styles.main}>
-      <div className={styles.principal}>
-        {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div>
-    </Page>
+    <SessionContext.Provider value={session}>
+      {!session.isLoggedIn ?
+        <Page className={styles.main}>
+          <div className={styles.principal}>
+            {products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+        </Page> :
+        ''
+      }
+    </SessionContext.Provider>
   )
 }
